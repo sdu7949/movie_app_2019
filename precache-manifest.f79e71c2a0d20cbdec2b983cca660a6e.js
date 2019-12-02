732b993f5f4f40a4159e4e4a29f4be90a1f68e44
username <user@email.mail>
2019-12-02 12:43:48 +0900

    Updates

---
+++ precache-manifest.f79e71c2a0d20cbdec2b983cca660a6e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e727b59d985067bdcca2869c8ff30cb",
+    "url": "/movie_app_2019/index.html"
+  },
+  {
+    "revision": "5140002abb9605746be0",
+    "url": "/movie_app_2019/static/css/main.eaba35a9.chunk.css"
+  },
+  {
+    "revision": "18e17f1c73cef50e947f",
+    "url": "/movie_app_2019/static/js/2.e25caf30.chunk.js"
+  },
+  {
+    "revision": "5140002abb9605746be0",
+    "url": "/movie_app_2019/static/js/main.56c7b450.chunk.js"
+  },
+  {
+    "revision": "26c5953caa52b1651c1d",
+    "url": "/movie_app_2019/static/js/runtime-main.60e209e0.js"
+  }
+]);
